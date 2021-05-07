aec97f714030f517821ab111ee091affd471a928
Erez <erezfree29@gmail.com>
2021-05-07 10:12:11 -0700

    fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
 
-  # all the users friends 
+  # all the users friends
   def friends
     friends_i_sent_invitation_to = Friendship.where(user_id: id, confirmed: true).pluck(:friend_id)
     friends_i_got_invitation_from = Friendship.where(friend_id: id, confirmed: true).pluck(:user_id)
@@ -20,19 +20,20 @@ class User < ApplicationRecord
     User.where(id: ids)
   end
 
-  # users that have yet to confirm friend requests 
+  # users that have yet to confirm friend requests
   def pending_friends
     ids = Friendship.where(user_id: id, confirmed: false).pluck(:friend_id)
     User.where(id: ids)
   end
+
   # Users who have requested to be friends
-  def friend_requests 
+  def friend_requests
     ids = Friendship.where(friend_id: id, confirmed: false).pluck(:user_id)
     User.where(id: ids)
   end
 
-   # check if user is a friend 
-   def friends_with(user)
-    Friendship.select(:friend_id).where(user_id:id).pluck(:friend_id).include?(user.id)
+  # check if user is a friend
+  def friends_with(user)
+    Friendship.select(:friend_id).where(user_id: id).pluck(:friend_id).include?(user.id)
   end
 end
