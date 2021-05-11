222548a6b686ea7f8d5dfee97c1a392aef2afec5
Erez <erezfree29@gmail.com>
2021-05-11 11:22:45 -0700

    add uniq constraint,add uniq constraint test in model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_185227) do
+ActiveRecord::Schema.define(version: 2021_05_11_181445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_185227) do
     t.bigint "friend_id"
     t.bigint "user_id"
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
--- spec/models/friendship.spec.rb
+++ spec/models/friendship.spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
   describe 'ActiveRecords Associations' do
+    it 'needs to have a unique combination of user_id and friend_id' do
+      expect { should validate_uniqueness_of(user_id).scoped_to(:friend_id) }
+    end
     it 'belongs to user' do
       expect { should belongs_to(:user) }
     end
